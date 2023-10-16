2959f715e8754d67861364efc69e44361daab690
lyoubov <lyoubovsid@gmail.com>
2023-10-16 20:57:22 +0300

    Добавлена звдвча 22

---
+++ task_022_Таблица_Квадратов_1_N/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая 
+принимает на вход число (N) и 
+выдает таблицу квадратов чисел от 1 до N 
+
+5 -> 1, 4, 9, 16, 25
+2 -> 1, 4                       */
+
+System.Console.WriteLine("Введите выбранный критерий: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num1; i = i + 1)
+{
+    System.Console.WriteLine($"{i} * {i} = {Math.Pow (i, 2)}");
+}
