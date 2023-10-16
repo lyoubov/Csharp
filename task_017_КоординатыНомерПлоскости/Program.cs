ae80e471b55d7f4bfb92d967d508a86006265e99
lyoubov <lyoubovsid@gmail.com>
2023-10-16 17:18:46 +0300

    добавлена задача 18

---
+++ task_017_КоординатыНомерПлоскости/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход 
+координаты точки (X и Y), при этом 
+X <> 0 и Y <> 0   и 
+выдает номер четверти плоскости в которой находится эта точка.
+х= 34,  у= -30   ->    4
+х= 2,   у= 4     ->    1
+х= -34, у=-30    ->    3        */
+
+System.Console.WriteLine("Введите данные X");
+int numX = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите данные Y");
+int numY = Convert.ToInt32(System.Console.ReadLine());
+
+if (numX > 0 && numY > 0)
+{
+    System.Console.WriteLine("1я четверть");
+}
+if (numX > 0 && numY < 0)
+{
+    System.Console.WriteLine("4я четверть");
+}
+if (numX < 0 && numY < 0)
+{
+    System.Console.WriteLine("3я четверть");
+}
---
+++ task_018_По_Четверти_диапазон/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая по заданному номеру четверти, показывает диапазон
+возможных координат точек в этой плоскости (x, y): */
+
+System.Console.WriteLine("Введите номер четверти:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    System.Console.WriteLine("X; Y > 0");
+}
+if (num == 2)
+{
+    System.Console.WriteLine("X > 0 и Y < 0");
+}
+if (num == 3)
+{
+    System.Console.WriteLine("X и Y  <  0");
+}
