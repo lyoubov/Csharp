188a79409ebb55641a1f34ced912e5352bec1d57
lyoubov <lyoubovsid@gmail.com>
2023-10-17 22:10:27 +0300

    изменения

--- task_019_полиндром_пятизнач/Program.cs
+++ task_019_полиндром_пятизнач/Program.cs
@@ -9,7 +9,7 @@
 12821 -> True
 234322 -> Число не пятизначное False */
 
-/*                      Вариант 1
+//                    Вариант 1
 System.Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int num1 = num / 10000;
@@ -33,14 +33,22 @@ if (num >= 10000 & num <= 99999)
 else
 {
     System.Console.WriteLine("не пятизначное");
-}                   */
+}                  
 
 // перевернуть число n
+/*
 System.Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int rev_N = 0;
 for (int i = N; i > 0; i = i / 10)
 {
     rev_N = rev_N * 10 + i % 10;
-    System.Console.WriteLine(rev_N);
+    // System.Console.WriteLine(rev_N);
 }
+if (N == rev_N)
+{
+    System.Console.WriteLine("True");
+}
+else
+    System.Console.WriteLine("False");
+    */
